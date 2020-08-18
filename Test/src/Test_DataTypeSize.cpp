976bf9e0e0c603c634206925d28ea4bed1c90d0b
Reto <reto271@gmx.ch>
2020-08-18 15:02:53 +0200

    Add data type size tests.

---
+++ Test/src/Test_DataTypeSize.cpp
@@ -0,0 +1,62 @@
+#include <gtest/gtest.h>
+#include <stdint.h>
+
+#include "Application/IOHandler.hpp"
+
+namespace
+{
+// using ::testing::StrictMock;
+// using ::testing::Return;
+// using ::testing::Exactly;
+// using ::testing::_;
+// using ::testing::HasSubstr;
+
+
+class Test_DataTypeSize : public testing::Test, public IOHandler
+{
+protected:
+    void SetUp() override
+    {
+    }
+
+    void TearDown() override
+    {
+    }
+
+};
+
+
+TEST_F(Test_DataTypeSize, trivialTypes)
+{
+    bool val = false;
+
+    EXPECT_EQ(1, sizeof(bool));
+
+    EXPECT_EQ(1, sizeof(char));
+    EXPECT_EQ(2, sizeof(short));
+    EXPECT_EQ(4, sizeof(int));
+    EXPECT_EQ(8, sizeof(long));
+
+    EXPECT_EQ(1, sizeof(unsigned char));
+    EXPECT_EQ(2, sizeof(unsigned short));
+    EXPECT_EQ(4, sizeof(unsigned int));
+    EXPECT_EQ(8, sizeof(unsigned long));
+
+    EXPECT_EQ(1, sizeof(int8_t));
+    EXPECT_EQ(2, sizeof(int16_t));
+    EXPECT_EQ(4, sizeof(int32_t));
+    EXPECT_EQ(8, sizeof(int64_t));
+
+    EXPECT_EQ(1, sizeof(uint8_t));
+    EXPECT_EQ(2, sizeof(uint16_t));
+    EXPECT_EQ(4, sizeof(uint32_t));
+    EXPECT_EQ(8, sizeof(uint64_t));
+}
+
+TEST_F(Test_DataTypeSize, customTypes)
+{
+    EXPECT_EQ(2, sizeof(m_debouncedSignals));
+    EXPECT_EQ(10, sizeof(m_debounceArray));
+}
+
+}  // unnamed namespace

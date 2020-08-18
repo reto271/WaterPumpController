d6216e7ff4b778eb1d50add22a2fafe77634c275
Reto <reto271@gmx.ch>
2020-08-18 09:19:08 +0200

    Add some infrastructure files
    Copied from the HeatingMonitor application. (Uncrustify and launch
    Emacs, incl gtags/global).

---
+++ scripts/uncr.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Change into the script directory
+SCRIPTDIR=$(readlink -f $(dirname "$0"))
+pushd "${SCRIPTDIR}" > /dev/null
+cd ..
+
+#../Common/scripts/showSW_Version.sh
+
+# Uncrustify the applicaiton files
+uncrustify -c Common/uncr.cfg Application/*.cpp Application/*.hpp --replace
+
+# Do only format the main.c (actually it is genererated and manually edited)
+uncrustify -c Common/uncr.cfg Core/Src/main.c --replace
+
+# Move backup files to temporary folder
+mkdir -p tmp
+mv Application/*.unc-backup~ tmp
+mv Application/*.unc-backup.md5~ tmp
+mv Core/Src/*.unc-backup~ tmp
+mv Core/Src/*.unc-backup.md5~ tmp
+
+# Back to the original location
+popd > /dev/null
+
+exit ${feedback}

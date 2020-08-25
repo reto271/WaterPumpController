657513483b5ef860bbc6cb318a8da0437b2848c9
Reto <reto271@gmx.ch>
2020-08-25 15:51:15 +0200

    Add timer create, cancel & expire functions to TimerMgr
    Extende the timer manager to create and expire timers. Add the necessary
    unit tests.

--- Application/TimerMgr.hpp
+++ Application/TimerMgr.hpp
@@ -16,10 +16,34 @@ public:
     bool is100ms();
     void confirm100ms();
 
+    uint32_t createTimer(const uint32_t timeoutInSec);
+    void cancelTimer(const uint32_t timerId);
+    bool isTimerExpired(const uint32_t timerId);
+
+    static const uint32_t INVALID_TIMER_ID = UINT32_MAX;
+
+protected:
+    void incrementTimerId();
+    uint32_t getCurrentTime();
+    bool freePositionInArray(uint32_t& freePos);
+
 private:
     bool m_exe10msInvervall;
     bool m_exe100msInvervall;
-    uint32_t m_periodCounter;
+    uint32_t m_periodCounter10ms;
+    uint32_t m_periodCounter100ms;
+
+    uint32_t m_currentTime;
+
+    typedef struct {
+        uint32_t timerId;
+        uint32_t timeItExpires;
+    } TimerData;
+    static const uint32_t MAX_CURRENT_ACTIVE_TIMERS = 10;
+
+    uint32_t m_nextFreeTimerId;
+    TimerData m_activeTimer[MAX_CURRENT_ACTIVE_TIMERS];
+
 };
 
 #endif // __TIMERMGR_HPP__

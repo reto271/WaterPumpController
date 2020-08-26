7df3439df4665e391b4367bcabf3b38acea7ad82
Reto <reto271@gmx.ch>
2020-08-26 10:36:10 +0200

    Add tests for pump controller
    - Test a full cycle off-on-off-on
    - Test switch off by timeout

--- Test/src/Test_PumpController.cpp
+++ Test/src/Test_PumpController.cpp
@@ -175,8 +175,8 @@ TEST_F(Test_PumpController, switchOnLaterOffByTimeout)
     m_pPumpCtrl->run();
 
     // Upper level signal no longer true
-    EXPECT_CALL(mockIOHdl, getLevelLow()).Times(5).WillRepeatedly(Return(false));
-    EXPECT_CALL(mockIOHdl, getLevelHigh()).Times(AtMost(5)).WillRepeatedly(Return(true));
+    EXPECT_CALL(mockIOHdl, getLevelLow()).Times(5).WillRepeatedly(Return(true));
+    EXPECT_CALL(mockIOHdl, getLevelHigh()).Times(AtMost(5)).WillRepeatedly(Return(false));
     EXPECT_CALL(mockIOHdl, setPumpState(true)).Times(5);
     EXPECT_CALL(mockIOHdl, setLED_State(true)).Times(5);
     EXPECT_CALL(mockTimerMgr, isTimerExpired(TIMER_ID_ON_TIME)).Times(5).WillRepeatedly(Return(false));
@@ -186,12 +186,12 @@ TEST_F(Test_PumpController, switchOnLaterOffByTimeout)
     m_pPumpCtrl->run();
     m_pPumpCtrl->run();
 
-    // Lower level signal no longer true -> switch off and start idle timeout
-    EXPECT_CALL(mockIOHdl, getLevelLow()).Times(1).WillRepeatedly(Return(false));
-    EXPECT_CALL(mockIOHdl, getLevelHigh()).Times(1).WillRepeatedly(Return(false));
+    // Lower level signal still on, timeout -> switch off and start idle timeout
+    EXPECT_CALL(mockIOHdl, getLevelLow()).Times(1).WillRepeatedly(Return(true));
+    EXPECT_CALL(mockIOHdl, getLevelHigh()).Times(AtMost(1)).WillRepeatedly(Return(false));
     EXPECT_CALL(mockIOHdl, setPumpState(false)).Times(4);
     EXPECT_CALL(mockIOHdl, setLED_State(false)).Times(4);
-    EXPECT_CALL(mockTimerMgr, cancelTimer(TIMER_ID_ON_TIME)).Times(1);
+    EXPECT_CALL(mockTimerMgr, isTimerExpired(TIMER_ID_ON_TIME)).Times(1).WillRepeatedly(Return(true));
     EXPECT_CALL(mockTimerMgr, createTimer(TEST_MIN_PUMP_IDLE_TIME)).Times(1).WillRepeatedly(Return(TIMER_ID_OFF_TIME));
     EXPECT_CALL(mockTimerMgr, isTimerExpired(TIMER_ID_OFF_TIME)).Times(3).WillRepeatedly(Return(false));
     m_pPumpCtrl->run();
@@ -200,5 +200,81 @@ TEST_F(Test_PumpController, switchOnLaterOffByTimeout)
     m_pPumpCtrl->run();
 }
 
+
+TEST_F(Test_PumpController, switchOnOfftestIdleTimeout)
+{
+    const uint32_t TIMER_ID_ON_TIME = 1;
+    const uint32_t TIMER_ID_OFF_TIME = 2;
+    const uint32_t TIMER_ID_ON_TIME_2 = 3;
+
+    // Both levels true -> switch on the pump
+    EXPECT_CALL(mockIOHdl, getLevelLow()).Times(1).WillRepeatedly(Return(true));
+    EXPECT_CALL(mockIOHdl, getLevelHigh()).Times(AtMost(1)).WillRepeatedly(Return(true));
+    EXPECT_CALL(mockIOHdl, setPumpState(true)).Times(1);
+    EXPECT_CALL(mockIOHdl, setLED_State(true)).Times(1);
+    EXPECT_CALL(mockTimerMgr, createTimer(TEST_MAX_PUMP_RUN_TIME)).Times(1).WillRepeatedly(Return(TIMER_ID_ON_TIME));
+//    EXPECT_CALL(mockTimerMgr, isTimerExpired(TIMER_ID_ON_TIME)).Times(2).WillRepeatedly(Return(false));
+    m_pPumpCtrl->run();
+
+    // Upper level signal no longer true
+    EXPECT_CALL(mockIOHdl, getLevelLow()).Times(1).WillRepeatedly(Return(false));
+    EXPECT_CALL(mockIOHdl, getLevelHigh()).Times(AtMost(1)).WillRepeatedly(Return(true));
+    EXPECT_CALL(mockIOHdl, setPumpState(true)).Times(1);
+    EXPECT_CALL(mockIOHdl, setLED_State(true)).Times(1);
+    EXPECT_CALL(mockTimerMgr, isTimerExpired(TIMER_ID_ON_TIME)).Times(1).WillRepeatedly(Return(false));
+    m_pPumpCtrl->run();
+
+    // Lower level signal no longer true -> switch off and start idle timeout
+    EXPECT_CALL(mockIOHdl, getLevelLow()).Times(1).WillRepeatedly(Return(false));
+    EXPECT_CALL(mockIOHdl, getLevelHigh()).Times(1).WillRepeatedly(Return(false));
+    EXPECT_CALL(mockIOHdl, setPumpState(false)).Times(2);
+    EXPECT_CALL(mockIOHdl, setLED_State(false)).Times(2);
+    EXPECT_CALL(mockTimerMgr, cancelTimer(TIMER_ID_ON_TIME)).Times(1);
+    EXPECT_CALL(mockTimerMgr, createTimer(TEST_MIN_PUMP_IDLE_TIME)).Times(1).WillRepeatedly(Return(TIMER_ID_OFF_TIME));
+    EXPECT_CALL(mockTimerMgr, isTimerExpired(TIMER_ID_OFF_TIME)).Times(1).WillRepeatedly(Return(false));
+    m_pPumpCtrl->run();
+    m_pPumpCtrl->run();
+
+    // Lower level signal on again
+    EXPECT_CALL(mockIOHdl, getLevelLow()).Times(0);    // no call, still in idle timeout
+    EXPECT_CALL(mockIOHdl, getLevelHigh()).Times(0);   // no call, still in idle timeout
+    EXPECT_CALL(mockIOHdl, setPumpState(false)).Times(2);
+    EXPECT_CALL(mockIOHdl, setLED_State(false)).Times(2);
+    EXPECT_CALL(mockTimerMgr, isTimerExpired(TIMER_ID_OFF_TIME)).Times(2).WillRepeatedly(Return(false));
+    m_pPumpCtrl->run();
+    m_pPumpCtrl->run();
+
+    // Idle timer expires
+    EXPECT_CALL(mockIOHdl, getLevelLow()).Times(1).WillRepeatedly(Return(false));
+    EXPECT_CALL(mockIOHdl, getLevelHigh()).Times(AtMost(1)).WillRepeatedly(Return(false));
+    EXPECT_CALL(mockIOHdl, setPumpState(false)).Times(2);
+    EXPECT_CALL(mockIOHdl, setLED_State(false)).Times(2);
+    EXPECT_CALL(mockTimerMgr, isTimerExpired(TIMER_ID_OFF_TIME)).Times(1).WillRepeatedly(Return(true));
+    m_pPumpCtrl->run();
+    m_pPumpCtrl->run();
+
+    // Lower level becomes true
+    EXPECT_CALL(mockIOHdl, getLevelLow()).Times(2).WillRepeatedly(Return(true));
+    EXPECT_CALL(mockIOHdl, getLevelHigh()).Times(2).WillRepeatedly(Return(false));
+    EXPECT_CALL(mockIOHdl, setPumpState(false)).Times(2);
+    EXPECT_CALL(mockIOHdl, setLED_State(false)).Times(2);
+    m_pPumpCtrl->run();
+    m_pPumpCtrl->run();
+
+    // Higher level becomes true
+    EXPECT_CALL(mockIOHdl, getLevelLow()).Times(5).WillRepeatedly(Return(true));
+    EXPECT_CALL(mockIOHdl, getLevelHigh()).Times(AtMost(5)).WillRepeatedly(Return(true));
+    EXPECT_CALL(mockIOHdl, setPumpState(true)).Times(5);
+    EXPECT_CALL(mockIOHdl, setLED_State(true)).Times(5);
+    EXPECT_CALL(mockTimerMgr, createTimer(TEST_MAX_PUMP_RUN_TIME)).Times(1).WillRepeatedly(Return(TIMER_ID_ON_TIME_2));
+    EXPECT_CALL(mockTimerMgr, isTimerExpired(TIMER_ID_ON_TIME)).Times(0);
+    EXPECT_CALL(mockTimerMgr, isTimerExpired(TIMER_ID_OFF_TIME)).Times(0);
+    EXPECT_CALL(mockTimerMgr, isTimerExpired(TIMER_ID_ON_TIME_2)).Times(4).WillRepeatedly(Return(false));
+    m_pPumpCtrl->run();
+    m_pPumpCtrl->run();
+    m_pPumpCtrl->run();
+    m_pPumpCtrl->run();
+    m_pPumpCtrl->run();
+}
 
 }  // unnamed namespace

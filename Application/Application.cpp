aeb06838debe885e8c61d1139a704224b16ab043
Reto <reto271@gmx.ch>
2020-09-16 08:39:14 +0200

    Prepare Command Interpreter
    Only a few crude steps

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -8,47 +8,50 @@
 #include "IOHandler.hpp"
 #include "PeriodicDump.hpp"
 #include "PumpController.hpp"
+#include "CommandInterpreter.hpp"
 
-static DebugWriter* pDbgWriter = NULL;
-static TimerMgr* pTimerMgr = NULL;
-static IOHandler* pIoHandler = NULL;
-static PeriodicDump* pPeriodicDump = NULL;
-static PumpController* pPumpCtrl = NULL;
+static DebugWriter* m_pDbgWriter = NULL;
+static TimerMgr* m_pTimerMgr = NULL;
+static IOHandler* m_pIoHandler = NULL;
+static PeriodicDump* m_pPeriodicDump = NULL;
+static PumpController* m_pPumpCtrl = NULL;
+// static CommandInterpreter* m_pCmdInter = NULL;
 
 void initializeBackgroundLoop(UART_HandleTypeDef* pUART_Hdl)
 {
-    pTimerMgr = new TimerMgr();
-    pDbgWriter = new DebugWriter(pUART_Hdl, pTimerMgr);
-    pIoHandler = new IOHandler();
-    pPeriodicDump = new PeriodicDump(pIoHandler, pTimerMgr, pDbgWriter);
-    pPumpCtrl = new PumpController(pIoHandler, pTimerMgr, pDbgWriter);
+    m_pTimerMgr = new TimerMgr();
+    m_pDbgWriter = new DebugWriter(pUART_Hdl, m_pTimerMgr);
+    m_pIoHandler = new IOHandler();
+    m_pPeriodicDump = new PeriodicDump(m_pIoHandler, m_pTimerMgr, m_pDbgWriter);
+    m_pPumpCtrl = new PumpController(m_pIoHandler, m_pTimerMgr, m_pDbgWriter);
+    // m_pCmdInter = new CommandInterpreter(pUART_Hdl);
 
-    pPeriodicDump->setPumpController(pPumpCtrl);
+    m_pPeriodicDump->setPumpController(m_pPumpCtrl);
 
-    pDbgWriter->print(" ");
-    pDbgWriter->print("Water Pump Controller");
-    pDbgWriter->print("V01.10 B03");
-    /// pDbgWriter->print("V01.09");
-    pDbgWriter->print("---");
+    m_pDbgWriter->print(" ");
+    m_pDbgWriter->print("Water Pump Controller");
+    m_pDbgWriter->print("V01.10 B04");
+    /// m_pDbgWriter->print("V01.09");
+    m_pDbgWriter->print("---");
 }
 
 void ApplicationTimerInterrupt10ms()
 {
-    pTimerMgr->timerISR();
+    m_pTimerMgr->timerISR();
 }
 
 void runBackgroudLoop()
 {
-    if(true == pTimerMgr->is10ms()) {
-        pTimerMgr->confirm10ms();
-        pIoHandler->run();
+    if(true == m_pTimerMgr->is10ms()) {
+        m_pTimerMgr->confirm10ms();
+        m_pIoHandler->run();
     }
-    if(true == pTimerMgr->is100ms()) {
-        pTimerMgr->confirm100ms();
-        pPumpCtrl->run();
+    if(true == m_pTimerMgr->is100ms()) {
+        m_pTimerMgr->confirm100ms();
+        m_pPumpCtrl->run();
     }
-    if(true == pTimerMgr->is1s()) {
-        pTimerMgr->confirm1s();
-        pPeriodicDump->run();
+    if(true == m_pTimerMgr->is1s()) {
+        m_pTimerMgr->confirm1s();
+        m_pPeriodicDump->run();
     }
 }

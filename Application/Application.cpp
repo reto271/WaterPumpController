749a912ce9456943c397c9411d2b28e1a7daebf6
Reto <reto271@gmx.ch>
2020-09-17 10:01:58 +0200

    Set version number to V01.10

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -30,8 +30,8 @@ void initializeBackgroundLoop(UART_HandleTypeDef* pUART_Hdl)
 
     m_pDbgWriter->print(" ");
     m_pDbgWriter->print("Water Pump Controller");
-    m_pDbgWriter->print("V01.10 B07");
-    /// m_pDbgWriter->print("V01.09");
+    //m_pDbgWriter->print("V01.10 B07");
+    m_pDbgWriter->print("V01.10");
     m_pDbgWriter->print("---");
 }
 
